b69f85d0eb9b0cd02383455f2aa1120d72761c8b
Christian Vanderwall <cvanderwall14@gmail.com>
2016-02-04 16:57:33 -0800

    Create State model.

--- address/models.py
+++ address/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Country(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
-    
+
+class State(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
--- address/tests/test_models.py
+++ address/tests/test_models.py
@@ -11,3 +11,11 @@ class CountryModelTest(TestCase):
         txt = str(self.country)
         self.assertEqual(txt, 'United States')
 
+
+class StateModelTest(TestCase):
+    def setUp(self):
+        self.state = State.objects.create(name='California')
+
+    def test_string_representation(self):
+        txt = str(self.state)
+        self.assertEqual(txt, 'California')
