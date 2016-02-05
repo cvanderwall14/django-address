2dad701d7f7a5cd9e47730d9ddac3beb3b53b3e7
Christian Vanderwall <cvanderwall14@gmail.com>
2016-02-04 18:09:48 -0800

    Create address model.

--- address/models.py
+++ address/models.py
@@ -9,21 +9,57 @@ class Country(models.Model):
 
 
 class State(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class PostalCode(models.Model):
-    value = models.CharField(max_length=20)
+    value = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.value
+
+
+class Address(models.Model):
+    street_number = models.CharField(max_length=10, blank=True, null=True)
+    street = models.CharField(max_length=100, blank=True, null=True)
+    city = models.ForeignKey(City, blank=True, null=True)
+    state = models.ForeignKey(State, blank=True, null=True)
+    postal_code = models.ForeignKey(PostalCode, blank=True, null=True)    
+    country = models.ForeignKey(Country, blank=True, null=True)
+    raw = models.CharField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        txt = ''
+        if self.street_number:
+            txt = self.street_number
+        if self.street:
+            if txt:
+                txt += ' '
+            txt += self.street
+        if self.city:
+            if txt:
+                txt += ', '
+            txt += str(self.city)
+        if self.state:
+            if txt:
+                txt += ', '
+            txt += str(self.state)
+        if self.postal_code:
+            if txt:
+                txt += ' '
+            txt += str(self.postal_code)
+        if self.country:
+            if txt:
+                txt += ', '
+            txt += str(self.country)
+        return txt
--- address/tests/test_models.py
+++ address/tests/test_models.py
@@ -37,3 +37,23 @@ class PostalCodeModelTest(TestCase):
     def test_string_representation(self):
         txt = str(self.postal_code)
         self.assertEqual(txt, '94123')
+
+
+class AddressModelTest(TestCase):
+    def setUp(self):
+        self.country = Country.objects.create(name='United States')
+        self.state = State.objects.create(name='CA')
+        self.city = City.objects.create(name='San Francisco')
+        self.postal_code = PostalCode.objects.create(value='94123')
+        self.address = Address.objects.create(
+            street_number='1234',
+            street='Main Street',
+            city=self.city,
+            postal_code=self.postal_code,
+            state=self.state,
+            country=self.country
+        )
+
+    def test_string_representation(self):
+        txt = str(self.address)
+        self.assertEqual(txt, '1234 Main Street, San Francisco, CA 94123, United States') 
