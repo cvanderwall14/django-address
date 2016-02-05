988909647bab84f0df2709dd085225a300c6010e
Christian Vanderwall <cvanderwall14@gmail.com>
2016-02-04 18:20:00 -0800

    Add hidden address fields to form.

--- address/forms.py
+++ address/forms.py
@@ -12,3 +12,10 @@ class AddressWidget(forms.TextInput):
 
 class AddressFormSimple(forms.Form):
     address = forms.CharField(label='Address', widget=AddressWidget(attrs={'onFocus': 'init()'}))
+    street_number = forms.CharField(widget=forms.HiddenInput(), required=False)
+    street = forms.CharField(widget=forms.HiddenInput(), required=False)
+    city = forms.CharField(widget=forms.HiddenInput(), required=False)
+    state = forms.CharField(widget=forms.HiddenInput(), required=False)
+    zip = forms.CharField(widget=forms.HiddenInput(), required=False)
+    country = forms.CharField(widget=forms.HiddenInput(), required=False)
+    
